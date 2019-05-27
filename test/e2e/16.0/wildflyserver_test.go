443f7d1917ac8a588d80ae4bcceeb15a2c18060b
Jeff Mesnil <jmesnil@gmail.com>
2019-05-27 16:49:09 +0200

    split 16 and 17 test

--- test/e2e/16.0/wildflyserver_test.go
+++ test/e2e/16.0/wildflyserver_test.go
@@ -17,7 +17,6 @@ import (
 	wildflyv1alpha1 "github.com/wildfly/wildfly-operator/pkg/apis/wildfly/v1alpha1"
 	wildflyframework "github.com/wildfly/wildfly-operator/test/framework"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 var (
@@ -39,49 +38,10 @@ func TestWildFlyServer(t *testing.T) {
 		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
 	}
 	// run subtests
-	t.Run("WildFlyServer", func(t *testing.T) {
-		t.Run("WildFly17", func(t *testing.T) {
-			t.Run("BasicTest", WildFly17BasicTest)
-		})
-		t.Run("WildFly16", func(t *testing.T) {
-			t.Run("BasicTest", WildFly16BasicTest)
-			//t.Run("ClusterTest", WildFlyClusterTest)
-		})
+	t.Run("WildFly16Server", func(t *testing.T) {
+		t.Run("BasicTest", WildFly16BasicTest)
+		t.Run("ClusterTest", WildFlyClusterTest)
 	})
-}
-
-func wildflyBasicServerScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, applicationTag string) error {
-	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		return fmt.Errorf("could not get namespace: %v", err)
-	}
-
-	name := "example-wildfly"
-	// create wildflyserver custom resource
-	wildflyServer := wildflyframework.MakeBasicWildFlyServer(namespace, name, "quay.io/jmesnil/wildfly-operator-quickstart:"+applicationTag, 1)
-	err = wildflyframework.CreateAndWaitUntilReady(f, ctx, t, wildflyServer)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Application %s is deployed with %d instance\n", name, 1)
-
-	context := goctx.TODO()
-
-	// update the size to 2
-	err = f.Client.Get(context, types.NamespacedName{Name: name, Namespace: namespace}, wildflyServer)
-	if err != nil {
-		return err
-	}
-	wildflyServer.Spec.Size = 2
-	err = f.Client.Update(context, wildflyServer)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Updated aplication %s size to %d\n", name, wildflyServer.Spec.Size)
-
-	// check that the resource have been updated
-	return wildflyframework.WaitUntilReady(f, t, wildflyServer)
 }
 
 func wildflyClusterViewTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
@@ -91,7 +51,7 @@ func wildflyClusterViewTest(t *testing.T, f *framework.Framework, ctx *framework
 	}
 
 	name := "clusterbench"
-	standaloneConfigXML, err := ioutil.ReadFile("test/e2e/standalone-clustering-test.xml")
+	standaloneConfigXML, err := ioutil.ReadFile("test/e2e/16.0/standalone-clustering-test.xml")
 	if err != nil {
 		return err
 	}
@@ -145,38 +105,7 @@ func wildflyClusterViewTest(t *testing.T, f *framework.Framework, ctx *framework
 }
 
 func WildFly16BasicTest(t *testing.T) {
-	WildFlyBasicTest(t, "16.0")
-}
-
-func WildFly17BasicTest(t *testing.T) {
-	WildFlyBasicTest(t, "17.0")
-}
-
-func WildFlyBasicTest(t *testing.T, applicationTag string) {
-	ctx := framework.NewTestCtx(t)
-	defer ctx.Cleanup()
-	err := ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
-	if err != nil {
-		t.Fatalf("failed to initialize cluster resources: %v", err)
-	}
-	t.Log("Initialized cluster resources")
-	namespace, err := ctx.GetNamespace()
-	fmt.Printf("Initialized cluster resources\n")
-	if err != nil {
-		t.Fatal(err)
-	}
-	// get global framework variables
-	f := framework.Global
-	// wait for wildfly-operator to be ready
-	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "wildfly-operator", 1, retryInterval, timeout)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("Operator is deployed\n")
-
-	if err = wildflyBasicServerScaleTest(t, f, ctx, applicationTag); err != nil {
-		t.Fatal(err)
-	}
+	wildflyframework.WildFlyBasicTest(t, "16.0")
 }
 
 func WildFlyClusterTest(t *testing.T) {
---
+++ test/framework/common.go
@@ -0,0 +1,73 @@
+package framework
+
+import (
+	goctx "context"
+	"fmt"
+	"testing"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// WildFlyBasicTest runs basic operator tests
+func WildFlyBasicTest(t *testing.T, applicationTag string) {
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	err := ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	if err != nil {
+		t.Fatalf("failed to initialize cluster resources: %v", err)
+	}
+	t.Log("Initialized cluster resources")
+	namespace, err := ctx.GetNamespace()
+	fmt.Printf("Initialized cluster resources\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// get global framework variables
+	f := framework.Global
+	// wait for wildfly-operator to be ready
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "wildfly-operator", 1, retryInterval, timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("Operator is deployed\n")
+
+	if err = wildflyBasicServerScaleTest(t, f, ctx, applicationTag); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func wildflyBasicServerScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, applicationTag string) error {
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		return fmt.Errorf("could not get namespace: %v", err)
+	}
+
+	name := "example-wildfly"
+	// create wildflyserver custom resource
+	wildflyServer := MakeBasicWildFlyServer(namespace, name, "quay.io/jmesnil/wildfly-operator-quickstart:"+applicationTag, 1)
+	err = CreateAndWaitUntilReady(f, ctx, t, wildflyServer)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Application %s is deployed with %d instance\n", name, 1)
+
+	context := goctx.TODO()
+
+	// update the size to 2
+	err = f.Client.Get(context, types.NamespacedName{Name: name, Namespace: namespace}, wildflyServer)
+	if err != nil {
+		return err
+	}
+	wildflyServer.Spec.Size = 2
+	err = f.Client.Update(context, wildflyServer)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Updated aplication %s size to %d\n", name, wildflyServer.Spec.Size)
+
+	// check that the resource have been updated
+	return WaitUntilReady(f, t, wildflyServer)
+}
