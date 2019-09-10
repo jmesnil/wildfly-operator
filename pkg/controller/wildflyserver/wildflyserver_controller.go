49cd071b21a946adc5a4f6d148030cf94f186931
Jeff Mesnil <jmesnil@gmail.com>
2019-09-10 10:53:02 +0200

    only update the StatefulSet spec and label

--- pkg/controller/wildflyserver/wildflyserver_controller.go
+++ pkg/controller/wildflyserver/wildflyserver_controller.go
@@ -158,7 +158,7 @@ func (r *ReconcileWildFlyServer) Reconcile(request reconcile.Request) (reconcile
 				foundStatefulSet.Spec.Template = statefulSet.Spec.Template
 				foundStatefulSet.Spec.Replicas = statefulSet.Spec.Replicas
 				foundStatefulSet.Annotations["wildfly.org/wildfly-server-generation"] = strconv.FormatInt(wildflyServer.Generation, 10)
-				if err = r.client.Update(context.TODO(), statefulSet); err != nil {
+				if err = r.client.Update(context.TODO(), foundStatefulSet); err != nil {
 					reqLogger.Error(err, "Failed to Update StatefulSet.", "StatefulSet.Namespace", foundStatefulSet.Namespace, "StatefulSet.Name", foundStatefulSet.Name)
 					return reconcile.Result{}, err
 				}
