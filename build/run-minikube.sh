d031a737417c666e4b1eeeca11c9e97e04d16490
Jeff Mesnil <jmesnil@redhat.com>
2020-07-21 11:02:07 +0200

    Delete all old content

--- build/run-minikube.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f deploy/service_account.yaml
-kubectl apply -f deploy/role.yaml
-kubectl apply -f deploy/role_binding.yaml
-# install WildFlyServer CRD
-kubectl apply -f deploy/crds/wildfly_v1alpha1_wildflyserver_crd.yaml
-# install WildFly Operator
-kubectl create -f deploy/operator.yaml
