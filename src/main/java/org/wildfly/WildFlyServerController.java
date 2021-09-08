f8c530c772d298a9c6d33432c6f3d3c01793558d
Jeff Mesnil <jmesnil@redhat.com>
2021-09-08 12:16:26 +0200

    Operator image (works only in default namespace)
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- src/main/java/org/wildfly/WildFlyServerController.java
+++ src/main/java/org/wildfly/WildFlyServerController.java
@@ -1,5 +1,7 @@
 package org.wildfly;
 
+import javax.inject.Inject;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.*;
 import io.javaoperatorsdk.operator.api.Context;
@@ -8,23 +10,17 @@ import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
 @Controller
 public class WildFlyServerController implements ResourceController<WildFlyServer> {
 
-    private final KubernetesClient client;
-
-    public WildFlyServerController(KubernetesClient client) {
-        this.client = client;
-    }
-
-    // TODO Fill in the rest of the controller
+    @Inject
+    KubernetesClient client;
 
     @Override
     public void init(EventSourceManager eventSourceManager) {
-        // TODO: fill in init
+        System.out.println("WildFlyServerController.init");
     }
 
     @Override
     public UpdateControl<WildFlyServer> createOrUpdateResource(
         WildFlyServer resource, Context<WildFlyServer> context) {
-        // TODO: fill in logic
 
         return UpdateControl.noUpdate();
     }
